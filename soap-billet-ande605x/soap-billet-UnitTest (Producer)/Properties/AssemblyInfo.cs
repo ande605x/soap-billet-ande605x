da077023cb939b4953881a317dd6e54753651ad9
Anders Thomsen <ande605x@edu.easj.dk>
2017-09-20 10:50:48 +0200

    Nu med UnitTests

---
+++ soap-billet-ande605x/soap-billet-UnitTest (Producer)/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("soap-billet-UnitTest (Producer)")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("soap-billet-UnitTest (Producer)")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9ad610a0-251c-4f4b-ad9d-7dbecffc0c66")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ soap-billet-ande605x/soap-billet-UnitTest (Producer)/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using soap_billet_ande605x;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace soap_billet_UnitTest__Producer_
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestPrisBilMedBrobizz()
+        {
+            //Arrange
+            BilletService bs = new BilletService();
+            //Act
+            int testtal = bs.PrisBilMedBrobizz();
+            //Assert
+            Assert.AreEqual(228,testtal);
+        }
+
+        [TestMethod]
+        public void PrisMCMedBrobizz()
+        {
+            //Arrange
+            BilletService bs = new BilletService();
+            //Act
+            int testtal = bs.PrisMCMedBrobizz();
+            //Assert
+            Assert.AreEqual(114, testtal);
+        }
+
+        [TestMethod]
+        public void PrisBilUdenBrobizz()
+        {
+            //Arrange
+            BilletService bs = new BilletService();
+            //Act
+            int testtal = bs.PrisBilUdenBrobizz();
+            //Assert
+            Assert.AreEqual(240, testtal);
+        }
+
+        [TestMethod]
+        public void PrisMCUdenBrobizz()
+        {
+            //Arrange
+            BilletService bs = new BilletService();
+            //Act
+            int testtal = bs.PrisMCUdenBrobizz();
+            //Assert
+            Assert.AreEqual(120, testtal);
+        }
+    }
+}
