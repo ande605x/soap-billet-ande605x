12cd0c5beba0df888ac580564e2492540e5bbc49
Anders Thomsen <ande605x@edu.easj.dk>
2017-09-25 15:04:22 +0200

    Test teamservices2

--- soap-billet-ande605x/soap-billet-ande605x/BilletService.svc.cs
+++ soap-billet-ande605x/soap-billet-ande605x/BilletService.svc.cs
@@ -7,7 +7,7 @@ using System.ServiceModel.Web;
 using System.Text;
 using BilletLib;
 
-namespace soap_billet_ande605x     // PRODUCER-
+namespace soap_billet_ande605x     // PRODUCER--
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "BilletService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select BilletService.svc or BilletService.svc.cs at the Solution Explorer and start debugging.
